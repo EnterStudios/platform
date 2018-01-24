3dd335adc3b4e558a17c99f916110cefc30f9b0d
Daria Voytova <vdariaclem@gmail.com>
2018-01-24 11:27:57 +0300

    Draw chart

--- apinf_packages/mqtt_dashboard/client/draw_chart.js
+++ apinf_packages/mqtt_dashboard/client/draw_chart.js
@@ -2,6 +2,10 @@
   This file is covered by the EUPL license.
   You may obtain a copy of the licence at
   https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+// Meteor packages imports
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
 import moment from 'moment';
 
 Template.drawChart.onRendered(function () {
@@ -65,31 +69,38 @@ Template.drawChart.onRendered(function () {
     const aggregatedData = Template.currentData().aggregatedData;
 
     if (aggregatedData) {
-      const chartType = Template.currentData().chartType;
+      const chartType = this.data.chartType;
 
-      if (chartType === 'real-time') {
-        aggregatedData.forEach(dataset => {
-          const date = moment(dataset.key).format('HH:mm:ss');
-
-          this.chart.data.labels.push(date);
-          this.chart.data.datasets[0].data.push(dataset.doc_count);
-        });
-      } else {
+      if (chartType === 'no-real-time') {
+        // If data is a point
         if (aggregatedData.length === 1) {
+          // Display point
           this.chart.data.datasets[0].pointRadius = 2;
         } else {
+          // Otherwise display line without point
           this.chart.data.datasets[0].pointRadius = 0;
         }
 
+        // Create a labels
         this.chart.data.labels = aggregatedData.map(dataset => {
           return moment(dataset.key).format('DD/MM');
         });
 
+        // Get chart dara
         this.chart.data.datasets[0].data = aggregatedData.map(dataset => {
           return dataset.doc_count;
         });
+      } else {
+        aggregatedData.forEach(dataset => {
+          const date = moment(dataset.key).format('HH:mm:ss');
+
+          // Add a new dataset to existing
+          this.chart.data.labels.push(date);
+          this.chart.data.datasets[0].data.push(dataset.doc_count);
+        });
       }
 
+      // Update chart
       this.chart.update();
     }
   });
